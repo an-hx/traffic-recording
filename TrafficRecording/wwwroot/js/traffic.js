ea4d95691fb6b58fbd1fd0230e6ad2337abb9245
An Ho <an.ho@groovetechnology.com>
2021-04-19 14:33:02 +0700

    update the ui

--- TrafficRecording/wwwroot/js/traffic.js
+++ TrafficRecording/wwwroot/js/traffic.js
@@ -19,7 +19,7 @@ connection.on("ReceiveMessage", function (message) {
     var methodCol = $("<td/>").text(message.method);
     var urlCol = $("<td/>").text(message.url);
     if (message.body.length) {
-        var jsonContent = "<pre class='json-content' id='json-content-" + count + "'>" + message.body + "</pre>";
+        var jsonContent = "<textarea class='json-content' id='json-content-" + count + "'>" + message.body + "</textarea>";
         jsonContent += "</br><a href='javascript:void(0)' id='btn-copy-" + count + "'>Copy</a>";
         var bodyCol = $("<td/>").append(jsonContent);
     } else {
@@ -35,31 +35,14 @@ connection.on("ReceiveMessage", function (message) {
     $('#http-log tbody').prepend(row);
     setTimeout(function () {
         document.getElementById('btn-copy-' + count).onclick = function (e) {
-            //$('#json-content-' + count).select();
-            const el = document.createElement('textarea');
-            el.className = 'hidden';
-            el.value = $('#json-content-' + count).text();
-            document.body.appendChild(el);
-            el.select();
-            document.execCommand('copy');
-            document.body.removeChild(el);
+            const input = document.getElementById('json-content-' + count);
+            // 2) Select the text
+            input.focus();
+            input.select();
+
+            // 3) Copy text to clipboard
+            const isSuccessful = document.execCommand('copy');
         };
-        document.getElementById('btn-copy-' + count).addEventListener('copy', function (e) {
-            e.preventDefault();
-            if (e.clipboardData) {
-                e.clipboardData.setData('text/plain', 'custom content');
-            } else if (window.clipboardData) {
-                window.clipboardData.setData('Text', 'custom content');
-            }
-        });
     }, 1000);
     
 });
-
-//function onCopyClipboard(count) {
-//    var $temp = $("<input class='hidden'>");
-//    $("body").append($temp);
-//    $temp.val($("#json-content-" + count).val()).select();
-//    document.execCommand("copy");
-//    $temp.remove();
-//}
